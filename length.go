c4833fba5bef28ec32fc8ae255bf16bfe0d8878b
emylincon <emylincon@gmail.com>
2021-12-28 10:56:50 +0000

    #3 added length functionality

---
+++ length.go
@@ -0,0 +1,32 @@
+package golist
+
+func (arr *List) Len() int {
+	switch arr.list.(type) {
+	case []int:
+		list := arr.list.([]int)
+		return len(list)
+
+	case []int32:
+		list := arr.list.([]int32)
+		return len(list)
+
+	case []int64:
+		list := arr.list.([]int64)
+		return len(list)
+
+	case []float32:
+		list := arr.list.([]float32)
+		return len(list)
+
+	case []float64:
+		list := arr.list.([]float64)
+		return len(list)
+
+	case []string:
+		list := arr.list.([]string)
+		return len(list)
+
+	default:
+		return -1
+	}
+}
